c815e83639c3e9e3528e67a7a230551284461a2e
federicoladereche <federicobrasilcuritiba@gmail.com>
2023-04-05 11:03:26 -0300

    feat(main): :building_construction: add session management
    Manage user sessions in backend.

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
 import { Logger } from '@nestjs/common';
+import * as session from 'express-session'
 
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
@@ -31,7 +32,21 @@ async function bootstrap() {
     }
     return new Logger(name);
   };
+
   const app = await NestFactory.create(AppModule);
+
+  /**
+   * Middleware for Express that provides session management
+   * It enables you to store user-specific data on the server-side, and associate it with a session ID
+   * 'my-secret' secret is used to sign the session ID cookie and ensure its integrity.
+   */
+  global.app.use(
+    session({
+      secret: process.env.SESSION_SECRET || 'my-secret',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
 
   await app.listen(port, () => {
     console.log(`Server is running in http://localhost:${port}`);
