10be3689527b4da0b0e24e195ff13765a1b32ff4
federicoladereche <federicobrasilcuritiba@gmail.com>
2023-04-14 11:47:13 -0300

    test(libs): :white_check_mark: add search engine tests
    - Search engine service and ealstic search service tests

--- test/libs/search-engine/search-engine.service.spec.ts
+++ test/libs/search-engine/search-engine.service.spec.ts
@@ -1,18 +1,93 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { SearchEngineService } from '../../../libs/search-engine/src/search-engine.service';
+import { Test, TestingModule } from "@nestjs/testing";
+import { PaginationArgs } from "../../../src/models/dtos/pagination.in.dto";
+import {
+  AggregationsAggregate,
+  SearchResponse,
+} from "@elastic/elasticsearch/lib/api/types";
+import { SearchEngineService } from "../../../libs/search-engine/src";
+import { ElasticSearchService } from "../../../libs/search-engine/src/services/elastic-search/elastic-search.service";
+import { SEARCH_ENGINE_CONFIG } from "../../../libs/search-engine/src/models/dtos/search-engine.constants";
 
-describe('SearchEngineService', () => {
+jest.mock("../../../libs/search-engine/src/services/elastic-search/elastic-search.service");
+
+describe("SearchEngineService", () => {
   let service: SearchEngineService;
+  let mockElasticSearchService: jest.Mocked<ElasticSearchService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SearchEngineService],
+      providers: [
+        SearchEngineService,
+        {
+          provide: SEARCH_ENGINE_CONFIG,
+          useValue: { provider: "elasticsearch", options: {} },
+        },
+      ],
     }).compile();
 
     service = module.get<SearchEngineService>(SearchEngineService);
+    mockElasticSearchService = new ElasticSearchService({}) as any;
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(service).toBeDefined();
+  });
+
+  it("should create an ElasticSearchService instance", () => {
+    expect(service["searchEngineClient"]).toBeInstanceOf(ElasticSearchService);
+  });
+
+  it("should throw an error for unsupported search engine client", () => {
+    const config = {
+      provider: "unsupported",
+      options: {},
+    };
+    const createService = () => new SearchEngineService(config as any);
+    expect(createService).toThrowError(
+      "Unsupported search engine client: unsupported"
+    );
+  });
+
+  it("should search using the ElasticSearchService", async () => {
+    const query = "test query";
+    const pagination: PaginationArgs = { skip: 0, limit: 10 };
+    const expectedResult:
+      | SearchResponse<unknown, Record<string, AggregationsAggregate>>
+      | any = {
+      body: {
+        hits: {
+          total: {
+            value: 1,
+            relation: "eq",
+          },
+          hits: [{ _source: { title: "Test title", content: "Test content" } }],
+        },
+      },
+      statusCode: 200,
+      headers: {},
+      meta: {
+        context: null,
+        name: "elasticsearch-js",
+        request: { params: {}, options: {}, id: null },
+        connection: { url: "http://localhost:9200", id: "http" },
+        attempts: 0,
+        aborted: false,
+      },
+    };
+
+    mockElasticSearchService.search.mockResolvedValue(expectedResult);
+    service["searchEngineClient"] = mockElasticSearchService;
+
+    const result = await service.search(query, pagination);
+
+    expect(mockElasticSearchService.search).toHaveBeenCalledWith(
+      query,
+      pagination
+    );
+    expect(result).toEqual(expectedResult);
+  });
+
+  it("should return the provider", () => {
+    expect(service.provider).toEqual("elasticsearch");
   });
 });
--- test/libs/search-engine/services/elastic-search/elastic-search.service.spec.ts
+++ test/libs/search-engine/services/elastic-search/elastic-search.service.spec.ts
@@ -1,18 +1,76 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { ElasticSearchService } from '../../../../../libs/search-engine/src/services/elastic-search/elastic-search.service';
+import { Test, TestingModule } from "@nestjs/testing";
+import { ElasticSearchService } from "../../../../../libs/search-engine/src/services/elastic-search/elastic-search.service";
+import { Client } from "@elastic/elasticsearch";
 
-describe('ElasticSearchService', () => {
+jest.mock("@elastic/elasticsearch");
+
+describe("ElasticSearchService", () => {
   let service: ElasticSearchService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ElasticSearchService],
+      providers: [
+        {
+          provide: ElasticSearchService,
+          useFactory: () => {
+            const mockedClient = new Client({ node: "http://localhost:9200" });
+            return new ElasticSearchService({ node: "http://localhost:9200", client: mockedClient });
+          },
+        },
+      ],
     }).compile();
 
+    (Client as any).mockClear();
     service = module.get<ElasticSearchService>(ElasticSearchService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it("should search using the Elasticsearch client", async () => {
+    const query = "test query";
+    const skip = 0;
+    const limit = 10;
+
+    const searchMock = jest.fn().mockResolvedValue({
+      body: {
+        hits: {
+          total: { value: 1, relation: "eq" },
+          hits: [{ _source: { title: "Test title", content: "Test content" } }],
+        },
+      },
+    });
+
+    (service as any).client.search = searchMock;
+
+    const result = await service.search(query, { skip, limit });
+
+    expect(searchMock).toHaveBeenCalledWith({
+      index: "search",
+      from: skip,
+      size: limit,
+      body: {
+        query: {
+          multi_match: {
+            query,
+            fields: ["title", "content"],
+          },
+        },
+      },
+    });
+    expect(result).toEqual({
+      body: {
+        hits: {
+          total: { value: 1, relation: "eq" },
+          hits: [{ _source: { title: "Test title", content: "Test content" } }],
+        },
+      },
+    });
+  });
+
+  it("should throw an error when searching in Elasticsearch fails", async () => {
+    const searchMock = jest.fn().mockRejectedValue(new Error("Search error"));
+    (service as any).client.search = searchMock;
+
+    await expect(
+      service.search("test query", { skip: 0, limit: 10 })
+    ).rejects.toThrow("Search error");
   });
 });
